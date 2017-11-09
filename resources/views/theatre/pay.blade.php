aa86f5a46a755fb379a2289bba5dd3a24e4c1bdf
YuanGuan <intelinker@163.com>
2017-11-10 00:09:13 +0800

    no message

--- resources/views/theatre/pay.blade.php
+++ resources/views/theatre/pay.blade.php
@@ -24,9 +24,9 @@
 <script src="{{ url('/src/js/jQuery.min.2.2.4.js') }}" ></script>
 
 <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" type="text/javascript" charset="utf-8"></script>
-{{--<script type="text/javascript" charset="utf-8">--}}
+<script type="text/javascript" charset="utf-8">
     wx.config({{ $js->config(array('chooseWXPay')) }});
-{{--</script>--}}
+</script>
 <script>
     $(document).ready(function() {
         document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
