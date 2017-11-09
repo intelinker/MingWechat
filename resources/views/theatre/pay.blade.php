d47161c9c323a9d76f27d16bcb699fe479c97525
YuanGuan <intelinker@163.com>
2017-11-10 02:08:11 +0800

    no message

--- resources/views/theatre/pay.blade.php
+++ resources/views/theatre/pay.blade.php
@@ -6,18 +6,18 @@
 </head>
 <body>
 
-{{--<h1>一盒火柴</h1>--}}
+<h1>一盒火柴</h1>
 
-{{--<div>--}}
-    {{--<p>您购买了“一盒火柴”，总价格： 0.01元。</p>--}}
-    {{--<p>数量：1盒。</p>--}}
-{{--</div>--}}
+<div>
+    <p>您购买了“一盒火柴”，总价格： 0.01元。</p>
+    <p>数量：1盒。</p>
+</div>
 
-{{--<hr>--}}
+<hr>
 
-{{--<div>--}}
-    {{--<input name="button" id="btnPay" type="button" value="支付"  onclick="pay($config)"/>--}}
-{{--</div>--}}
+<div>
+    <input name="button" id="btnPay" type="button" value="支付"  onclick="pay($json)"/>
+</div>
 
 
 
@@ -31,10 +31,10 @@
     $(document).ready(function() {
 //        document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
             if( typeof WeixinJSBridge === 'undefined' ) {
-                alert('请在微信在打开页面！');
+                alert('请在微信打开页面！');
                 return false;
             }
-            WeixinJSBridge.invoke('getBrandWCPayRequest',str,function(res){
+            WeixinJSBridge.invoke('getBrandWCPayRequest',"{{$json}}",function(res){
 
                 WeixinJSBridge.log(res.err_msg);
                 //alert(res.err_code+res.err_desc+res.err_msg);
@@ -116,29 +116,32 @@
             {{--}--}}
         {{--});--}}
     });
-    {{--function pay($config) {--}}
-        {{--alert("pay");--}}
-        {{--wx.chooseWXPay({--}}
-            {{--timestamp: "{{$config['timestamp']}}", // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符--}}
-            {{--nonceStr: '{{$config['nonceStr']}}', // 支付签名随机串，不长于 32 位--}}
-            {{--package: '{{$config['package']}}', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）--}}
-            {{--signType: '{{$config['signType']}}', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'--}}
-            {{--paySign: '{{$config['paySign']}}', // 支付签名--}}
-            {{--success: function (res) {--}}
-                {{--// 支付成功后的回调函数--}}
-                {{--if(res.err_msg == "get_brand_wcpay_request：ok" ) {--}}
-                    {{--alert('支付成功。');--}}
-                    {{--window.location.href="{{url("wechat/pay_ok")}}";--}}
-                {{--}else{--}}
-                    {{--//alert(res.errMsg);--}}
-                    {{--alert("支付失败，请返回重试。");--}}
-                {{--}--}}
-            {{--},--}}
-            {{--fail: function (res) {--}}
-                {{--alert("支付失败，请返回重试。");--}}
-            {{--}--}}
-        {{--});--}}
-    {{--}--}}
+    function pay($config) {
+//        alert("pay");
+        if( typeof WeixinJSBridge === 'undefined' ) {
+            alert('请在微信打开页面！');
+            return false;
+        }
+        WeixinJSBridge.invoke('getBrandWCPayRequest',"{{$json}}",function(res){
+
+            WeixinJSBridge.log(res.err_msg);
+            //alert(res.err_code+res.err_desc+res.err_msg);
+            switch (res.err_msg){
+                case 'get_brand_wcpay_request:cancel':
+                    alert('用户取消支付！');
+                    break;
+                case 'get_brand_wcpay_request:fail':
+                    alert('支付失败！（'+res.err_desc+'）');
+                    break;
+                case 'get_brand_wcpay_request:ok':
+                    alert('支付成功！');
+                    break;
+                default:
+                    alert(JSON.stringify("{{$json}}"));
+                    break;
+            }
+        });
+    }
 
     {{--$(function(){--}}
 
