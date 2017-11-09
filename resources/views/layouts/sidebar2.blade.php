c76c64606bc3587937719d153b4c043d10eb4777
YuanGuan <intelinker@163.com>
2017-11-09 10:54:25 +0800

    wechat fixed

--- resources/views/layouts/sidebar2.blade.php
+++ resources/views/layouts/sidebar2.blade.php
@@ -4,6 +4,8 @@
             <li class="list-group-item {{ (Request::is('/') ? 'active' : '') }}"> <a href="{{ url('/') }}"><i class="fa fa-files-o fa-fw"></i> 文章管理 </a></li>
 
             <li class="list-group-item {{ (Request::is('*history') ? 'active' : '') }}"><a href="{{ url('admin/history') }}"> <i class="fa fa-files-o fa-fw"></i>座位设置</a></li>
+
+            <li class="list-group-item {{ (Request::is('*history') ? 'active' : '') }}"><a href="{{ url('admin/history') }}"> <i class="fa fa-files-o fa-fw"></i>订单管理</a></li>
         </ul>
     </div>
 </div>
