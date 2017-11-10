e0babfcb12ba5e514d5cf8934337ce10923963dc
YuanGuan <intelinker@163.com>
2017-11-10 15:15:21 +0800

    api example

--- resources/views/theatre/pay.blade.php
+++ resources/views/theatre/pay.blade.php
@@ -246,7 +246,7 @@
         {{--};--}}
 
         document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-            WeixinJSBridge.invoke('getBrandWCPayRequest', 
+            WeixinJSBridge.invoke('getBrandWCPayRequest',
                 JSON.parse(JSON.stringify({
                     "appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //
                     "timeStamp":'{{$nonceStr}}', //$config['timestamp'],         //时间戳，自1970年以来的秒数
