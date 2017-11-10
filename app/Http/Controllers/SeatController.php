0bfe96cbe23ec850ada18acf6af4be3536387c7a
YuanGuan <intelinker@163.com>
2017-11-10 10:08:21 +0800

    no message

--- app/Http/Controllers/SeatController.php
+++ app/Http/Controllers/SeatController.php
@@ -227,7 +227,7 @@ class SeatController extends Controller
 //                 "timestamp"=> $config['timestamp']
 //            ];
             $json = $payment->configForPayment($prepayId); // 返回 json 字符串，如果想返回数组，传第二个参数 false
-            dd($json);
+//            dd($json);
             return view('theatre/pay',['js'=>$js,'config'=>$config, 'json'=>$json]);
         } else {
             var_dump($result);
