e4aee193cfe59e4dfe2de0520e504adbf9998876
YuanGuan <intelinker@163.com>
2017-11-12 19:56:40 +0800

    no message

--- resources/views/theatre/pay.blade.php
+++ resources/views/theatre/pay.blade.php
@@ -14,7 +14,7 @@
                 "appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //
                 "nonceStr":'{{$nonceStr}}', //$config['nonceStr'], //随机串
                 "package":'{{$package}}', //$config['package'],
-                "paySign":'{{$paySign}}'//$config['paySign'], //微信签名
+                "paySign":'{{$paySign}}',//$config['paySign'], //微信签名
                 "signType":'{{$signType}}', //$config['signType'],         //微信签名方式：
                 "timeStamp":'{{$nonceStr}}', //$config['timestamp'],         //时间戳，自1970年以来的秒数
             }
