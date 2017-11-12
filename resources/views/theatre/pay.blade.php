bba7c2962cbb9e024612018875882fe65a73a4cb
YuanGuan <intelinker@163.com>
2017-11-12 19:28:09 +0800

    no message

--- resources/views/theatre/pay.blade.php
+++ resources/views/theatre/pay.blade.php
@@ -16,7 +16,7 @@
 <hr>
 
 <div align="center">
-    <button style="width:210px; height:50px; border-radius: 15px;background-color:#FE6714; border:0px #FE6714 solid; cursor: pointer;  color:white;  font-size:16px;" type="button" onclick="callpay('{!! $json !!}')" >立即支付</button>
+    <button style="width:210px; height:50px; border-radius: 15px;background-color:#FE6714; border:0px #FE6714 solid; cursor: pointer;  color:white;  font-size:16px;" type="button" onclick="callpay()" >立即支付</button>
 </div>
 
 
@@ -233,7 +233,51 @@
 
 
 
-    function callpay($config)
+    {{--function callpay($config)--}}
+    {{--{--}}
+        {{--$config = {--}}
+                    {{--"appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //--}}
+                    {{--"timeStamp":'{{$nonceStr}}', //$config['timestamp'],         //时间戳，自1970年以来的秒数--}}
+                    {{--"nonceStr":'{{$nonceStr}}', //$config['nonceStr'], //随机串--}}
+                    {{--"package":'{{$package}}', //$config['package'],--}}
+                    {{--"signType":'{{$signType}}', //$config['signType'],         //微信签名方式：--}}
+                    {{--"paySign":'{{$paySign}}'//$config['paySign'], //微信签名--}}
+                {{--}--}}
+{{--//        alert(JSON.stringify($config));--}}
+        {{--document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {--}}
+            {{--WeixinJSBridge.invoke('getBrandWCPayRequest', $config--}}
+                {{--JSON.parse(JSON.stringify({--}}
+                    {{--"appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //--}}
+                    {{--"timeStamp":'{{$nonceStr}}', //$config['timestamp'],         //时间戳，自1970年以来的秒数--}}
+                    {{--"nonceStr":'{{$nonceStr}}', //$config['nonceStr'], //随机串--}}
+                    {{--"package":'{{$package}}', //$config['package'],--}}
+                    {{--"signType":'{{$signType}}', //$config['signType'],         //微信签名方式：--}}
+                    {{--"paySign":'{{$paySign}}'//$config['paySign'], //微信签名--}}
+                {{--}))--}}
+                {{--,function(res){--}}
+
+                {{--WeixinJSBridge.log(res.err_msg);--}}
+                {{--//alert(res.err_code+res.err_desc+res.err_msg);--}}
+                {{--switch (res.err_msg){--}}
+                    {{--case 'get_brand_wcpay_request:cancel':--}}
+                        {{--alert('用户取消支付！');--}}
+                        {{--break;--}}
+                    {{--case 'get_brand_wcpay_request:fail':--}}
+                        {{--alert('支付失败！（'+JSON.stringify($config)+'）');--}}
+                        {{--break;--}}
+                    {{--case 'get_brand_wcpay_request:ok':--}}
+                        {{--alert('支付成功！');--}}
+                        {{--break;--}}
+                    {{--default:--}}
+                        {{--alert('ok');--}}
+                        {{--break;--}}
+                {{--}--}}
+            {{--}--}}
+            {{--);--}}
+        {{--}, false);--}}
+
+    //调用微信JS api 支付
+    function jsApiCall()
     {
         $config = {
                     "appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //
@@ -243,38 +287,29 @@
                     "signType":'{{$signType}}', //$config['signType'],         //微信签名方式：
                     "paySign":'{{$paySign}}'//$config['paySign'], //微信签名
                 }
-//        alert(JSON.stringify($config));
-        document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-            WeixinJSBridge.invoke('getBrandWCPayRequest', $config
-                {{--JSON.parse(JSON.stringify({--}}
-                    {{--"appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //--}}
-                    {{--"timeStamp":'{{$nonceStr}}', //$config['timestamp'],         //时间戳，自1970年以来的秒数--}}
-                    {{--"nonceStr":'{{$nonceStr}}', //$config['nonceStr'], //随机串--}}
-                    {{--"package":'{{$package}}', //$config['package'],--}}
-                    {{--"signType":'{{$signType}}', //$config['signType'],         //微信签名方式：--}}
-                    {{--"paySign":'{{$paySign}}'//$config['paySign'], //微信签名--}}
-                {{--}))--}}
-                ,function(res){
-
+        WeixinJSBridge.invoke(
+            'getBrandWCPayRequest',
+            $config,
+            function(res){
                 WeixinJSBridge.log(res.err_msg);
-                //alert(res.err_code+res.err_desc+res.err_msg);
-                switch (res.err_msg){
-                    case 'get_brand_wcpay_request:cancel':
-                        alert('用户取消支付！');
-                        break;
-                    case 'get_brand_wcpay_request:fail':
-                        alert('支付失败！（'+JSON.stringify($config)+'）');
-                        break;
-                    case 'get_brand_wcpay_request:ok':
-                        alert('支付成功！');
-                        break;
-                    default:
-                        alert('ok');
-                        break;
-                }
+                alert(res.err_code+res.err_desc+res.err_msg);
             }
-            );
-        }, false);
+        );
+    }
+
+    function callpay()
+    {
+        if (typeof WeixinJSBridge == "undefined"){
+            if( document.addEventListener ){
+                document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+            }else if (document.attachEvent){
+                document.attachEvent('WeixinJSBridgeReady', jsApiCall);
+                document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+            }
+        }else{
+            jsApiCall();
+        }
+    }
     }
 </script>
 </body>
