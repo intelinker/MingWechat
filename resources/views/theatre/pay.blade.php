5b0481fd34402af0f4f951ec0a490cd2940b96e7
YuanGuan <intelinker@163.com>
2017-11-12 19:37:05 +0800

    no message

--- resources/views/theatre/pay.blade.php
+++ resources/views/theatre/pay.blade.php
@@ -3,6 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <title>购买支付</title>
+    <script type="text/javascript">
+        //调用微信JS api 支付
+        function jsApiCall()
+        {
+            $config = {
+                "appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //
+                "timeStamp":'{{$nonceStr}}', //$config['timestamp'],         //时间戳，自1970年以来的秒数
+                "nonceStr":'{{$nonceStr}}', //$config['nonceStr'], //随机串
+                "package":'{{$package}}', //$config['package'],
+                "signType":'{{$signType}}', //$config['signType'],         //微信签名方式：
+                "paySign":'{{$paySign}}'//$config['paySign'], //微信签名
+            }
+            WeixinJSBridge.invoke(
+                'getBrandWCPayRequest',
+                $config,
+                function(res){
+                    WeixinJSBridge.log(res.err_msg);
+                    alert(res.err_code+res.err_desc+res.err_msg);
+                }
+            );
+        }
+
+        function callpay()
+        {
+            alert({!! $config !!})
+            if (typeof WeixinJSBridge == "undefined"){
+                if( document.addEventListener ){
+                    document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
+                }else if (document.attachEvent){
+                    document.attachEvent('WeixinJSBridgeReady', jsApiCall);
+                    document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
+                }
+            }else{
+                jsApiCall();
+            }
+        }
+    </script>
 </head>
 <body>
 
@@ -29,41 +66,7 @@
 </script>
 <script>
 
-    //调用微信JS api 支付
-    function jsApiCall()
-    {
-        $config = {
-            "appId": 'wxd2ff9ea209f500d0',//$config['appId'],     //公众号名称，由商户传入 , //
-            "timeStamp":'{{$nonceStr}}', //$config['timestamp'],         //时间戳，自1970年以来的秒数
-            "nonceStr":'{{$nonceStr}}', //$config['nonceStr'], //随机串
-            "package":'{{$package}}', //$config['package'],
-            "signType":'{{$signType}}', //$config['signType'],         //微信签名方式：
-            "paySign":'{{$paySign}}'//$config['paySign'], //微信签名
-        }
-        WeixinJSBridge.invoke(
-            'getBrandWCPayRequest',
-            $config,
-            function(res){
-                WeixinJSBridge.log(res.err_msg);
-                alert(res.err_code+res.err_desc+res.err_msg);
-            }
-        );
-    }
 
-    function callpay()
-    {
-        alert({!! $config !!})
-        if (typeof WeixinJSBridge == "undefined"){
-            if( document.addEventListener ){
-                document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
-            }else if (document.attachEvent){
-                document.attachEvent('WeixinJSBridgeReady', jsApiCall);
-                document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
-            }
-        }else{
-            jsApiCall();
-        }
-    }
 
     $(document).ready(function() {
         callpay('{!! $json !!}');
